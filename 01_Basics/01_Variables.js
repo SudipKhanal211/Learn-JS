e18e78ba8d2fbef8de57c7019adbb7d21a9ecbf4
Sudip Khanal <sudipkhanal211@gmail.com>
2024-08-19 11:03:59 +0000

    Commit Save

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -9,21 +9,28 @@ Where,
     Var is used inside a function .
     Let and const can be used globally once decleared.
 */
+// {
+//     let firstName= "Alex";
+//     var lastName ="Sharma";
+//     const City = "Dang";
+// }
 
-let firstName= "Alex";
-var lastName ="Sharma";
-const City = "Dang";
+// Error will be thrown to let and Const Because declearation is done inside block.
+
 
 // let's update values of variables.
 
 firstName= "Sudip";
 lastName ="Khanal";
-// City ="Kathmandu"; 
-// City will through error as it is const and vaalue cannot be changes 
+City ="Kathmandu"; 
+// City will through error if block code is Run as it is const and value cannot be changes 
 
 
 console.log(firstName);
 console.log(lastName);
 console.log(City);
 
-// NOTE: WE AVOID USING var TO DECLEAR VARIABLE BECAUSE OF SCOPED ISSUE.
+
+
+/* NOTE: WE AVOID USING var TO DECLEAR VARIABLE BECAUSE OF SCOPED ISSUE.
+     Var cannot used outside function. */
