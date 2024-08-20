23c5cc6e9761d5f08164693dbdbf7af42eb951fe
Sudip Khanal <sudipkhanal211@gmail.com>
2024-08-20 19:14:59 +0000

    datatypeConversion_Operations

--- 01_Basics/03_datatypeConversion.js
+++ 01_Basics/03_datatypeConversion.js
@@ -27,3 +27,43 @@ like the same way there is many keyword that helps to convert datatype from one 
 2. Number(variable) is used to convert any datatype to number.
 3. Boolean(variable) is used to convert any datatype to boolean, and many more.
 */
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
