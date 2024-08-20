a0a96266917e7892627cb0aa4116771076e1a6bc
Sudip Khanal <sudipkhanal211@gmail.com>
2024-08-20 18:20:07 +0000

    Commit saved

---
+++ 01_Basics/03_datatypeConversion.js
@@ -0,0 +1,29 @@
+/*
+Datatype conversion os the peocess which conatins certain keywords to change the type of data.  
+    for example: 
+    // let age= 20
+// console.log(typeof (age));  => this will give number as the output.
+
+// lets store age in another variable and change its data type.
+// convertAge= age; 
+// convertAge = (String(age)); =>'String(age)' is used to convert data type of age to string.
+// console.log(typeof convertAge); 
+    
+
+*/
+
+let age= 20
+// checking datatype of age.
+ console.log(typeof (age));  
+
+// lets store age in another variable and change its data type.
+convertAge= age; 
+convertAge = (String(age));
+console.log(typeof convertAge); 
+
+/*
+like the same way there is many keyword that helps to convert datatype from one to another.
+1. String(variable) is used to convert any datatype to String. 
+2. Number(variable) is used to convert any datatype to number.
+3. Boolean(variable) is used to convert any datatype to boolean, and many more.
+*/
