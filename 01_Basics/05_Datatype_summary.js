ddf7a405e03e1bc1956c42a53d4a05b537d1e919
Sudip Khanal <sudipkhanal211@gmail.com>
2024-08-21 19:06:33 +0000

    Datatpye Summary

---
+++ 01_Basics/05_Datatype_summary.js
@@ -0,0 +1,25 @@
+/* 
+This file is about summary of data type.
+
+Type of data are classified in two catagory:
+
+1. Primitive datatype:
+    a.String
+    b.Number
+    c.Boolean
+    d.undefined
+    e.null
+    f.Symbol(Not used much in development)
+    g.BigInt(Not used much in development)
+
+2. Non-Primirive or refrence datatype:
+    a. Array
+    b. Object 
+    c. Function 
+    Note: type of array is object.
+
+
+
+NOTE:
+    While changing the value of primitive datatype it make changes on the copy of the orginal data, BUT Reference type make changes in orginal data/memory.
+*/
