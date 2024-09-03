b479fa993ea9a52a3e3a0ec677a4192441a5802b
Sudip Khanal <sudipkhanal211@gmail.com>
2024-09-03 00:30:51 +0000

    Function

---
+++ 03_Basics/01_Function.js
@@ -0,0 +1,22 @@
+// Function is the collection of code that perfrom specific task when invoked.
+
+// Lets create function that says my name:
+
+function myName() {
+    console.log("My name is Sudip Khanal.")
+}
+// function invoked:
+myName();
+
+// Write the function that add 2 numbers together:
+
+function sum(num1,num2) {
+    // num1 and num2 are parameters.
+    result= num1+num2
+    console.log(result)
+    return result
+    
+}
+// 2,3 are arguments which goes in params.
+sum(2,3)
+
