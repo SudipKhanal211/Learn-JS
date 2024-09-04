575499d36f423f5c08c8fd27b48111688c3fb5cd
Sudip Khanal <sudipkhanal211@gmail.com>
2024-09-04 17:19:45 +0000

    Function

--- 03_Basics/01_Function.js
+++ 03_Basics/01_Function.js
@@ -15,8 +15,36 @@ function sum(num1,num2) {
     result= num1+num2
     console.log(result)
     return result
-    
+        
 }
 // 2,3 are arguments which goes in params.
 sum(2,3)
 
+
+// ________________Function with objects and array_______________.
+
+function cartPrice(...num1) {
+    return num1
+}
+console.log(cartPrice(100,200,600,899))
+
+// NOTE: ...num1 collect all the arguments and store it in array 
+
+
+// Objects in function
+
+const User = {
+    Name: "Sudip",
+    Bill: 799
+};
+
+function checkObject(anyObject) {
+    // Just log the name from the passed object
+    console.log(`${anyObject.Name}`);
+}
+
+// Call the function with the User object
+checkObject(User);
+
+
+// 
