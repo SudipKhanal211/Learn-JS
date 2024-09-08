58b695e126bae26f1efdc4daf5a02731c8b420ea
Sudip Khanal <sudipkhanal211@gmail.com>
2024-09-08 09:30:31 +0000

    arrow function

---
+++ 03_Basics/03_Arrow_function.js
@@ -0,0 +1,19 @@
+// arrow function: a concise way to write function expressions good for simple function. syntax: (params)=> codes
+
+// function that print hello world:
+
+hello= ()=> console.log("Hello World!")
+// we can write code without curly brackets it it is only one line.
+hello()
+
+// Function to sum 2 numbers:
+
+sum =(num1,num2)=> console.log(num1+ num2);
+sum(3,7)
+
+// Function that says hello to the user.
+greet=()=>{ 
+    user ="Sudip"
+    console.log(`hello ${user}`)
+}
+greet()
